be31addcac513085d1f839f9c230b4c371358a49
Gonzalo <Gonzalo@ILoveRedHeads>
2017-12-16 20:01:35 +0100

    Commit para subir

---
+++ Boletin13.1/src/boletin13/ConversorTemperaturas.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin13;
+
+/**
+ *
+ * @author Gonzalo
+ */
+public class ConversorTemperaturas {
+    final float grados = 80;
+    
+    public float centigradosAFahrenheit(float centigrados) throws Excepcion{
+        float fahrenheit;
+        if(centigrados > 80){
+            throw new Excepcion();
+        } else {
+            fahrenheit = (float) (9.0 / 5.0 * centigrados + 32.4);
+            return fahrenheit;
+
+}
+    }
+    public float centigradorAReamur(float centigrados) throws Excepcion{
+        float reamur = 0;
+        if(centigrados>80){
+            throw new Excepcion();
+        }else{
+            reamur = (float)(4.0/5.0*centigrados);
+            return reamur;
+        }
+    }
+}
