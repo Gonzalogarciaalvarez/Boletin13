09c4fe95c7d6ba970567acd80a51665917c02b7e
Gonzalo <Gonzalo@ILoveRedHeads>
2017-12-16 20:01:40 +0100

    Commit para subir

---
+++ Boletin13.1/src/boletin13/Excepcion.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin13;
+
+/**
+ *
+ * @author Gonzalo
+ */
+public class Excepcion  extends Exception{
+    public Excepcion(){
+        
+    super("A temperatura non pode ser maior de 80º C");
+}
+}
